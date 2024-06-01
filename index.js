c9034474e14fcda8a21c0d9b5a1a050cbf92f587
Sreeram-1 <sreeramdevendra52@gmail.com>
2024-06-02 02:03:51 +0530

    feat: Initial commit of user and book data
    This commit initializes the project by adding JSON data files for users and books.
    
    The following files have been added:
    
    - data/books.json
    - data/users.json
    
    These files contain data for books and users, respectively. The data includes fields such as ID, name, author, genre, price, and subscription type.
    
    This data will be used for testing and development purposes.

--- index.js
+++ index.js
@@ -1,16 +1,97 @@
 const express = require("express");
-
+// Json data Import
+const {users} = require("./data/users.json")
 const app = express();
 
 const PORT = 8081;
 
 app.use(express.json());
 
+/**
+* route : /users
+method : get
+description: get all users
+acess: public
+parameters:none
+*/
+
+app.get("/users",(req,res)=>{
+    res.status(200).json({
+        success:true,
+        data:users,
+    });
+});
+
+app.get("/users/:id", (req,res)=>{
+    const {id} = req.params;
+    const user = users.find((each) => each.id===id);
+    if(!user){
+        return res.status(404).json({
+            success:false,
+            message:"user not found"
+            });
+            }
+
+           return res.status(200).json({
+            message : "true",
+            data : user,
+           }) 
+        })
+
 app.get("/",(req,res)=>{
     res.status(200).json({
         message: "server is up and running"
     })
 })
+
+app.post("/users", (req,res)=>{
+    const {id,name,surname,email,subscriptionType, subscriptionDate} = req.body;
+    const user = users.find((each)=> each.id===id)
+    if(user){
+       return res.status(404).json({
+            success:false,
+            message:"user exist with this id"
+        })
+    }
+  users.push({
+    id,
+    name,
+    surname,
+    email,
+    subscriptionType,
+    subscriptionDate
+  })
+  res.status(200).json({
+    sucess : true,
+    data :users,
+
+  })
+});
+
+app.put("/users/:id",(req,res)=>{
+    const {id} = req.params;
+    const {data} = req.body;
+    const user = users.find((each) => each.id===id)
+
+    if(!user)
+        return res.status(404).json({success:false, message:"user not found"})
+    const updatedUser=users.map((each)=>{
+        if(each.id===id){
+            return{
+                ...each,
+                ...data,
+            }
+        }
+        return each
+    })
+  return res.status(200).json({
+    success : true,
+    message : " user is updated successfully",
+    data : updatedUser,
+  })
+})
+
+
 app.get("*", (req,res)=>{
     res.status(404).json({
         message: "Route not found"
